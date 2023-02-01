67a43a8a174f3a402a8bebbfc89eb59a0f23a90c
prater12 <pranavprkash@gmail.com>
2023-02-01 22:17:37 +0530

    imagine knowing what this stuff does

--- src/constants.h
+++ src/constants.h
@@ -1,17 +1,60 @@
 #include <Arduino.h>
+#include <Servo.h>
+#include <NewPing.h>
+
 #ifndef MS_CONSTANTS
 #define MS_CONSTANTS
 
 int const mainEnable = 1;
 
+struct Iterator
+{
+    unsigned int delayMS;
+    unsigned long prevIterTimestamp;
+
+    Iterator(int d)
+    {
+        delayMS = d;
+        prevIterTimestamp = 0;
+    };
+    Iterator(int d, unsigned long nts)
+    {
+        delayMS = d;
+        prevIterTimestamp = nts;
+    };
+};
+
 namespace Iteration
 {
-    int const Line = 72;
+    Iterator Line(36);
+    Iterator Ping(50);
+}
+
+namespace Setup
+{
+    int botMode = 0; // Line, Line with obj detection, Fire detection, Water with fire detection, Remote Control
+    int const LINE = 0;
+    int const LINE_WITH_OBJ = 1;
+    int const FIRE = 2;
+    int const FIRE_WITH_WATER = 3;
+    int const RC = 4;
+
+    int innerSide = 0;
+    int const LEFT = 0;
+    int const RIGHT = 180;
+
+    int const BUTTON_PIN = A0;
+    unsigned long lastLeft = 0;
+
+    Iterator Blink(0);
+    int currentLEDState = LOW;
+    unsigned int const DELAYS[5] = {0, 500, 1000, 3000, 50000};
+
 }
 
 namespace Motor
 {
-    int const Pins[2][3] = {{7, 6, 5}, {2, 4, 3}};
+    int const Pins[2][3] = {{8, 7, 5}, {2, 4, 6}};
     int const Configs[3][2] = {{LOW, LOW}, {LOW, HIGH}, {HIGH, LOW}};
 
     int const STOP = 0;
@@ -23,14 +66,54 @@ namespace Motor
     int const SPEED = 120;
     int const SLOW_SPEED = SPEED * (0.8);
     int const FAST_SPEED = 200;
-    int const TURN_SPEED_DELTA = 20;
+    int const TURN_SPEED_DELTA = 50;
 
-    int const TURN_LIMIT = 5;
+    int const SHARP_TURN_LIMIT = 10000;
 }
 
 namespace IR
 {
-    int const Pins[3] = {A2, A1, A0};
+    int const Pins[3] = {A2, A1};
 }
+
+namespace Ultrasonic
+{
+    int const TRIGGER = A3;
+    int const ECHO = 13;
+    int const MAX_DISTANCE = 30;
+
+    NewPing sensor(TRIGGER, ECHO, MAX_DISTANCE);
+
+    int const SERVO_PIN = 9;
+    Servo servo;
+
+    int const servoPositions[5] = {30, 60, 90, 120, 150};
+    unsigned long readings[5] = {40, 40, 40, 40, 40};
+
+    int posIndex = 3;
+    int diff = 0;
+
+    int checkForObject()
+    {
+        return (readings[posIndex] <= 30 && readings[posIndex] > 0);
+    }
+
+    void writeReading()
+    {
+        readings[posIndex] = sensor.ping_result / US_ROUNDTRIP_CM; // Ping returned, uS result in ping_result, convert to cm with US_ROUNDTRIP_CM.
+    }
+
+    void moveServo()
+    {
+        if (posIndex >= 5 || posIndex <= 0)
+        {
+            diff *= -1;
+        }
+
+        posIndex += diff;
+        servo.write(servoPositions[posIndex]);
+    }
+
+} // namespace name
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,115 @@
 #include "constants.h"
-// #include <NewPing.h> NewPing NewPing(TRIG, ECHO, MAX_DISTANCE)
+
+int finishedSetup;
+
+unsigned long currentTimestamp;
 
 int irRead;
 
 char currentDirection = 'S';
-char prevTurn;
-int motorNowTurning = 0;
-
-unsigned long nextIterTime = 0;
-unsigned long currentTime;
+int motorSetReturn = 0;
+int takeSharpTurns = 0;
+unsigned long sharpTurnStart = 0;
 
 void pinModeAll(int const arr[], int size, int pin_mode);
 
-int setMotors(char state);
+int setMotors(char direction, int force);
 void setMotorState(int motor, int state);
 void setMotorSpeed(int motor, int speed);
+
+void echoCheck();
+
+void IR_ReadCycle();
 
 void setup()
 {
   // IR Setup
   pinModeAll(IR::Pins, 2, INPUT);
 
+  // Setup.. setup
+  pinMode(Setup::BUTTON_PIN, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+
   // BO Setup
   pinModeAll(Motor::Pins[0], 3, OUTPUT);
   pinModeAll(Motor::Pins[1], 3, OUTPUT);
+
+  delay(1000);
+
+  Serial.begin(9600);
+
+  Setup::lastLeft = millis();
+
+  while (!finishedSetup)
+  {
+    if (!digitalRead(Setup::BUTTON_PIN))
+    {
+      currentTimestamp = millis();
+
+      if (currentTimestamp - Setup::lastLeft > 5000)
+      {
+        finishedSetup = 1;
+      }
+      else if (currentTimestamp - Setup::lastLeft > 100)
+      {
+        if (Setup::botMode == 4)
+        {
+          Setup::botMode = 0;
+        }
+        else
+        {
+          Setup::botMode++;
+        }
+
+        Setup::Blink.delayMS = Setup::DELAYS[Setup::botMode];
+      }
+
+      Setup::lastLeft = millis();
+    }
+
+    // Blink
+    if (Setup::botMode == 4)
+    {
+      digitalWrite(LED_BUILTIN, LOW);
+    }
+    else if (Setup::botMode == 0)
+    {
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+    else if (currentTimestamp - Setup::Blink.prevIterTimestamp >= Setup::Blink.delayMS)
+    {
+      Setup::currentLEDState = 1 - Setup::currentLEDState;
+      digitalWrite(LED_BUILTIN, Setup::currentLEDState);
+      Setup::Blink.prevIterTimestamp = currentTimestamp;
+    }
+
+    Serial.println(Setup::botMode);
+  }
 }
 
 void loop()
 {
 
-  irRead = (digitalRead(IR::Pins[0]) << 1) | digitalRead(IR::Pins[1]);
+  currentTimestamp = millis();
 
-  motorNowTurning = setMotors(Motor::MovementDirection[irRead]);
+  IR_ReadCycle();
+}
 
-  if (motorNowTurning)
+void IR_ReadCycle()
+{
+
+  irRead = (digitalRead(IR::Pins[0]) << 1) | digitalRead(IR::Pins[1]); //(PORTC && 0b110) >> 1;
+
+  motorSetReturn = setMotors(Motor::MovementDirection[irRead], 0);
+
+  if (motorSetReturn == 1)
   {
-    nextIterTime = millis() + Iteration::Line;
+    Iteration::Line.prevIterTimestamp = currentTimestamp;
+  }
+
+  if (motorSetReturn)
+  {
+    currentDirection = Motor::MovementDirection[irRead];
   }
 }
 
@@ -58,42 +132,46 @@ void setMotorSpeed(int motor, int speed)
   analogWrite(Motor::Pins[motor][2], speed);
 }
 
-int setMotors(char direction)
+int setMotors(char direction, int force)
 {
-  if (direction != currentDirection)
-  {
 
+  if (direction != currentDirection || force)
+  {
     if (direction == 'S')
     {
-      currentDirection = direction;
+
       setMotorState(0, Motor::STOP);
       setMotorState(1, Motor::STOP);
+      return 2;
     }
     else if (direction == 'R')
     {
-      currentDirection = direction;
+
       setMotorSpeed(0, Motor::FAST_SPEED);
       setMotorSpeed(1, Motor::FAST_SPEED);
+
       setMotorState(0, Motor::BACKWARD);
       setMotorState(1, Motor::FORWARD);
       return 1;
     }
     else if (direction == 'L')
     {
-      currentDirection = direction;
+
       setMotorSpeed(0, Motor::FAST_SPEED);
       setMotorSpeed(1, Motor::FAST_SPEED);
+
       setMotorState(0, Motor::FORWARD);
       setMotorState(1, Motor::BACKWARD);
       return 1;
     }
-    else if (millis() >= nextIterTime)
+    else if (currentTimestamp - Iteration::Line.prevIterTimestamp >= Iteration::Line.delayMS)
     {
-      currentDirection = direction;
+
       setMotorSpeed(0, Motor::SPEED);
       setMotorSpeed(1, Motor::SPEED);
       setMotorState(0, Motor::FORWARD);
       setMotorState(1, Motor::FORWARD);
+      return 2;
     }
   }
 
